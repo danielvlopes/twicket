c95ae630a0576b106857b941f6176e8cbc870db8
Daniel Lopes <danielvlopes@gmail.com>
2010-03-01 17:02:39 -0300

    change to httparty

--- lib/twitter_raffle.rb
+++ lib/twitter_raffle.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
-require 'net/http'
+require 'httparty'
 require 'json/pure'
 require 'cgi'
 
 module Twitter
   class Raffle
+    include HTTParty
+    base_uri 'search.twitter.com'
+
     attr_reader :twittes, :hashtag, :winner
 
     def initialize(tag)
@@ -17,27 +20,24 @@ module Twitter
     def hashtag=(value)
       @hashtag = (value =~ /^#/) ? value : "##{value}"
     end
-    
+
     def empty?
       @twittes.empty?
     end
 
   private
     def search_url
-      "http://search.twitter.com/search.json?rpp=100&q=" + CGI::escape(hashtag)
+      "/search.json?rpp=100&q=" + CGI::escape(hashtag)
     end
 
     def next_page(page)
-      retrieve("http://search.twitter.com/search.json#{page}")
+      retrieve "/search.json#{page}"
     end
 
     def retrieve(url)
-      response = Net::HTTP.get_response(URI.parse(url))
-      result   = JSON.parse(response.body)
-
-      result["results"].each { |r| @twittes << r }
-      next_page(result["next_page"]) if result["next_page"]
-
+      response = self.class.get(url)
+      response["results"].each { |r| @twittes << r }
+      next_page(response["next_page"]) if response["next_page"]
     rescue JSON::ParserError => e
       raise "Json parse error, probably corrupt data."
     rescue => e
